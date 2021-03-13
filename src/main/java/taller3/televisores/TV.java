8427238deaa67a8f9daf3e333692f795d6c09f43
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-03-13 17:43:00 -0500

    Add files via upload

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,148 @@
+package taller3.televisores;
+
+public class TV {
+	Marca marca;
+	int canal = 1;
+	int precio = 500;
+	boolean estado;
+	int volumen = 1;
+	Control control;
+	static int numTV;
+	
+	public TV(Marca marca,boolean estado ) {
+		this.marca = marca;
+		this.estado = estado;
+		numTV++;
+	}
+	
+	// el get y set de marca
+	public Marca getMarca() {
+		return this.marca;
+	}
+	
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	
+	// el get y set de control
+	public Control getControl() {
+		return this.control;
+	}
+	
+	public void setControl(Control control) {
+		this.control = control;
+	}
+
+	
+	// el get y set de precio
+	public int getPrecio() {
+		return this.precio;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	// el get y set de volumen
+	public int getVolumen() {
+		return this.volumen;
+	}
+			
+	public void setVolumen(int volumen) {
+		
+		this.volumen = volumen;
+		
+			
+		
+		
+	}
+
+	// el get y set de canal
+	public int getCanal() {
+		return this.canal;
+	}
+			
+	public void setCanal(int canal) {
+		
+		this.canal = canal;
+		
+			
+		
+	}
+
+
+	//metodos turnON y turnOff
+	public void turnOn() {
+		this.estado = true;
+	}
+	
+	public void turnOff() {
+		this.estado = false;
+	}
+
+	public boolean getEstado(){
+		return this.estado;
+	}
+	
+	//metodos de aumentar/subir el canal/volumen
+	
+	public void canalUp(){
+		
+		if(estado == true) {
+			if( 1<=canal && canal<=120) {
+				canal++;
+		
+			}
+		}
+		else {
+			
+			
+		}
+		
+	}
+
+	public void canalDown(){
+		if(estado == true) {
+			if( 1<=canal&& canal<=120) {
+				canal--;
+		
+			}
+		}
+		
+		else {
+			
+		}
+		
+	
+	
+	}
+
+	public void volumenUp(){
+		volumen++;
+		
+	}
+
+	public void volumenDown(){
+		volumen--;
+		
+	}
+	
+	
+	
+	/**
+	 * 
+	 */
+	public static void setNumTV(int numtv){
+		numTV = numtv;
+		
+	}
+	
+	public static int getNumTV() {
+		return numTV;
+		
+	}
+	
+	
+	
+}
